37a4068b1889e0ddb20ec860219a4a24a3a27776
Mohammad Alwazzan <kingwazzan2@gmail.com>
2024-06-29 19:24:39 +0300

    Added the category and product types

---
+++ types/category/types.d.ts
@@ -0,0 +1,8 @@
+export interface CategoryType {
+  _id: string;
+  name: string;
+  description: string;
+  active: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
---
+++ types/product/type.d.ts
@@ -0,0 +1,18 @@
+export interface Metadata {
+  price: number;
+  size: string;
+  inStock: number;
+}
+
+export interface ProductType {
+  _id: string;
+  categoryId: string;
+  name: string;
+  description: string;
+  active: boolean;
+  createdAt: string;
+  updatedAt: string;
+  metadata: MetaData[];
+  totalStock: number;
+  imageURL: string;
+}
