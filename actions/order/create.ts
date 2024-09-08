168ea8a0f659a43da696d4b18b7df9f29107e631
Mohammad Alwazzan <kingwazzan2@gmail.com>
2024-09-08 04:22:33 +0300

    Added the create order method

--- actions/order/create.ts
+++ actions/order/create.ts
@@ -1,6 +1,22 @@
 import validation from '@/validation/order/create';
+import { CreateParams } from '@/types/order';
+import { flattenError } from '@/validation/handlers/flattenErrors';
+import { Order } from '@/helpers';
 
-const create = (state: any, data: FormData): ServerActionResponse => {
+const create = async (data: CreateParams): ServerActionResponse => {
   try {
-  } catch (error) {}
+    const validationResult = await validation.safeParseAsync(data);
+
+    if (!validationResult.success)
+      return { errors: flattenError(validationResult.error), success: false };
+    const orderClient = new Order();
+
+    const res = await orderClient.create(validationResult.data);
+
+    return { success: true, data: res, message: res.message };
+  } catch (error: any) {
+    return { success: false, message: error?.message ?? '' };
+  }
 };
+
+export { create };
