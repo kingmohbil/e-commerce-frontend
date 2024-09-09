2b32c4ccecace7f8dcb295aeac22ddf575c5fec3
Mohammad Alwazzan <kingwazzan2@gmail.com>
2024-09-09 03:34:22 +0300

    Removed the cart option for non logged in users

--- app/category/[id]/page.tsx
+++ app/category/[id]/page.tsx
@@ -4,6 +4,7 @@ import common from '@/config/languages/en/common.json';
 import { ProductCard, NoItemsInCart } from '@/components';
 import Category from '@/helpers/category';
 import text from '@/config/languages/en/category.json';
+import { getSession } from '@/utils/auth';
 
 export interface PageProps {
   params: {
@@ -17,6 +18,8 @@ export interface PageProps {
 const LoadProducts = async ({ id }: { id: string }) => {
   const products = await new Category().getProductsById(id);
 
+  const session = getSession();
+
   return (
     <>
       <h1 className="text-4xl font-semibold capitalize">{products.category.name}</h1>
@@ -25,8 +28,8 @@ const LoadProducts = async ({ id }: { id: string }) => {
         {products.products.map((product) => (
           <ProductCard
             key={product._id}
-            cart
             actionText={common.button.addToCart}
+            cart={!!session}
             className="max-w-56 sm:max-w-[400px]"
             product={{
               ...product,
